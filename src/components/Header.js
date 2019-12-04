dc3e91040c79ebb27216937eba22b9bcec2f4787
PatBateman89 <feric89@gmail.com>
2019-12-04 23:54:38 +0900

    header created, button animated

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = (props) => (
+  <div>
+    <header className="header">
+      <div className="logo-box">
+        <img src="/img/ub.png" alt="Logo" className="logo"/>
+      </div>
+
+      <h1 className="heading-primary">
+        <span className="heading-primary-main">UB Free Tour</span>
+        <span className="heading-primary-sub">one foot forward</span>
+        <div className="button-holder">
+          <span className="space"></span>
+          <a href="#" className="btn btn-white btn-animated" onClick={props.click}>Discover Our Tours</a>
+          <span className="space"></span>
+        </div>
+      </h1>
+    </header>
+  </div>
+)
+
+export default Header
