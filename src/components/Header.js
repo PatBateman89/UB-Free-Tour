37c8cddec40dfc30c92466b1ea8894bab45560d8
PatBateman89 <feric89@gmail.com>
2019-12-05 02:31:57 +0900

    sass added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Header = (props) => (
   <div>
     <header className="header">
-      <div className="logo-box">
-        <img src="/img/ub.png" alt="Logo" className="logo"/>
+      <div className="header__logo-box">
+        <img src="/img/ub.png" alt="Logo" className="header__logo"/>
       </div>
 
       <h1 className="heading-primary">
-        <span className="heading-primary-main">UB Free Tour</span>
-        <span className="heading-primary-sub">one foot forward</span>
-        <div className="button-holder">
+        <span className="heading-primary--main">UB Free Tour</span>
+        <span className="heading-primary--sub">one foot forward</span>
+        <div className="button--holder">
           <span className="space"></span>
-          <a href="#" className="btn btn-white btn-animated" onClick={props.click}>Discover Our Tours</a>
+          <a href="#" className="btn btn--white btn--animated" onClick={props.click}>Discover Our Tours</a>
           <span className="space"></span>
         </div>
       </h1>
